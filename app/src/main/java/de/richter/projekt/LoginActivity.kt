b9151a098051a8ff7d4202be4ef4ee706c50cde3
J3nsRicht3r <hohrus@gmx.de>
2020-09-02 16:00:33 +0200

    intecration Firebase, login view

--- app/src/main/java/de/richter/projekt/LoginActivity.kt
+++ app/src/main/java/de/richter/projekt/LoginActivity.kt
@@ -2,10 +2,21 @@ package de.richter.projekt
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    lateinit var currentUser: FirebaseUser
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+        currentUser = auth.currentUser!!
     }
+
 }
